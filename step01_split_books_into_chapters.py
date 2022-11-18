f0bb6a984d42e0611c2471888be80103ff5d2e4e
David Shapiro <daveshap37@gmail.com>
2022-11-18 09:25:39 -0500

    BOOKS HAVE BEEN SPLIT INTO CHAPTERS

---
+++ step01_split_books_into_chapters.py
@@ -0,0 +1,49 @@
+import os
+import re
+
+
+def openfile(filepath):
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        return infile.read()
+
+
+def savefile(filepath, content):
+    with open(filepath, 'w', encoding='utf-8') as outfile:
+        outfile.write(content)
+
+
+#watchwords = ['EBOOK', 'PREFACE', 'CONTENTS', '\.\s+\.\s+\.\s+', 'TRANSCRIBER']
+#watchwords = ['EBOOK', 'PREFACE', 'CONTENTS'] #, '\.\s+\.\s+\.\s+']
+watchwords = []
+
+
+if __name__ == '__main__':
+    files = os.listdir('books/')
+    for file in files:
+        book = openfile('books/%s' % file)
+        book = book.split('START OF THE PROJECT GUTENBERG')[1]
+        book = book.split('END OF THE PROJECT GUTENBERG')[0]
+        chapters = book.split('CHAPTER')
+        result = list()
+        for chapter in chapters:
+            chunks = re.split('(\*\s{4,})+\*', chapter)
+            result += chunks
+        result2 = list()
+        for r in result:
+            chunks = re.split('\s{4,}[XVIVI]+\s{4,}', r)
+            result2 += chunks
+        idx = 0
+        for chapter in result2:
+            #### SKIP IF TOO SHORT
+            chapter = chapter.strip()
+            if len(chapter) < 2000:
+                continue
+            #### COMPILE FILENAME & SAVE
+            if idx<10:
+                filename = file.replace('.txt', '_00%s.txt' % str(idx))
+            elif idx<100:
+                filename = file.replace('.txt', '_0%s.txt' % str(idx))
+            else:
+                filename = file.replace('.txt', '_%s.txt' % str(idx))
+            idx += 1
+            savefile('chapters/%s' % filename, chapter.strip())
