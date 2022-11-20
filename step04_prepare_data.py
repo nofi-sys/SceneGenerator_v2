d48c29e09762134697925c25cb766ea1ee8d3233
David Shapiro <daveshap37@gmail.com>
2022-11-20 07:22:04 -0500

    ready for finetuning. data cleanup should be complete.

---
+++ step04_prepare_data.py
@@ -0,0 +1,27 @@
+import os
+import json
+
+
+prompt_dir = 'prompts/'
+completion_dir = 'chapter_bits/'
+
+
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        return infile.read()
+
+
+if __name__ == '__main__':
+    files = os.listdir(prompt_dir)
+    data = list()
+    for file in files:
+        prompt = open_file(prompt_dir + file).strip()
+        completion = open_file(src_dir2 + file).strip()
+        prompt = prompt + '\n\nSCENE: '  # add demarc
+        completion = ' ' + completion + ' THE END'  # add stop token
+        info = {'prompt': prompt, 'completion': completion}
+        data.append(info)
+    with open('scenes.jsonl', 'w') as outfile:
+        for i in data:
+            json.dump(i, outfile)
+            outfile.write('\n')
